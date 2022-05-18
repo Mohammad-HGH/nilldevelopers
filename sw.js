9cd03c71a520017a223513cf70632bc060a612e2
mohammad-hgh <88213502+Mohammad-HGH@users.noreply.github.com>
2022-05-18 20:22:36 +0430

    add meta tags & web app ver

---
+++ sw.js
@@ -0,0 +1,50 @@
+const cacheName = "Nill-v1";
+const staticAssets = [
+  "./",
+  "./index.html",
+  "./assets/css/plugins.css",
+  "./assets/css/style.css",
+  "./assets/js/jquery-3.1.1.min.js",
+  "./assets/js/plugins.js",
+  "./assets/js/dsn-grid.js",
+  "./assets/js/custom.js",
+];
+
+self.addEventListener("install", async (e) => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", async (e) => {
+  const req = e.request;
+  const url = new URL(req.url);
+
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkAndCache(req));
+  }
+});
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    await cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached;
+  }
+}
